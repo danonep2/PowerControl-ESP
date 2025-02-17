a47536558c4dd12ed7c65e9c15798fe18aef9c89
iagoamorimuitec <68704070+danonep2@users.noreply.github.com>
2025-02-16 21:25:32 -0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,85 @@
+#include <EmonLib.h>
+#include <ESP8266WiFi.h> 
+#include <PubSubClient.h>
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+EnergyMonitor emon1;
+
+float ruido = 0.05;
+int tensao = 220;
+
+
+char ssid[] = "Sua red";
+char pass[] = "Senha da rede";
+const char* mqtt_server = "Server MQTT IP";
+const int mqtt_port = 1883;
+
+#define rele 5
+
+void callback(char* topic, byte* payload, int length) {
+  
+}
+
+void reconnect() {
+  // Loop até conectar ao broker MQTT
+  while (!client.connected()) {
+    Serial.print("Conectando ao MQTT...");
+    if (client.connect("ESP8266Client")) {
+      Serial.println("Conectado!");
+      // Inscreva-se em um tópico, se necessário
+      // client.subscribe("seu_topico");
+    } else {
+      Serial.print("Falha, rc=");
+      Serial.print(client.state());
+      Serial.println(" Tentando novamente em 5 segundos...");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Conectando à rede Wi-Fi: ");
+    Serial.println(ssid);
+    delay(1000);
+  }
+
+  Serial.println("Conectado ao Wi-Fi!");
+
+  pinMode(rele, OUTPUT);
+  digitalWrite(rele, HIGH);
+
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+
+  emon1.current(A0, 9.78); // Pino de entrada e calibração
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+
+  client.loop();
+
+  double Irms = emon1.calcIrms(1480);
+
+  if (Irms <= ruido) {
+    Irms = 0;
+  }
+
+  double consumo = Irms * tensao;
+
+  Serial.print("Irms:");
+  Serial.print(Irms);
+  Serial.print(" A   Consumo:");
+  Serial.print(consumo);
+  Serial.println(" W");
+
+  client.publish("consumption/ESP8266Client", String(consumo).c_str());
+  delay(1000);
+}
